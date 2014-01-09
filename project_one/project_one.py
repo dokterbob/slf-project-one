75dc768ebe529239017326539bba8db1d8aa58e0
cphilip <philip.crivelli@slf.ch>
2014-01-09 16:57:27 +0100

    "commit try"

--- project_one/project_one.py
+++ project_one/project_one.py
@@ -1,8 +1,10 @@
 from import_data import import_data
 from process import process_data
 from process import rotate_data
+from output import display_data
 
 # Read the data
 data = import_data('data.txt')
 data = process_data(data)
 data = rotate_data(data)
+display_data(data)
