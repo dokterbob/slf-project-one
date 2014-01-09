12b23d8501b896b3135a6370c0374ec6c013f575
Anselm Köhler <default@Anselm-Kohlers-MacBook.local>
2014-01-09 15:06:02 +0100

    run rotate function

--- project_one/project_one.py
+++ project_one/project_one.py
@@ -1,7 +1,8 @@
 from import_data import import_data
 from process import process_data
-
+from process import rotate_data
 
 # Read the data
 data = import_data('data.txt')
 data = process_data(data)
+data = rotate_data(data)
