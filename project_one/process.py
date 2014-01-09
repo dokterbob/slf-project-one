fixing normalization

project_one/process.py
@@ -1,6 +1,32 @@
 import numpy as np
 
 
+def normnumber(value):
+    """
+    Process data. Expects a list of lists with numbers.
+    * normalises a number
+    """
+
+    if value == -9999:
+        value = 0
+    else:
+        value /= 9999
+
+    return value
+
+normnumber = np.vectorize(normnumber)
+
+def normalize(data):
+    """
+    Process data. Expects a list of lists with numbers.
+
+    * apply normnumber function to the array
+    """
+
+    data = normnumber(data)
+
+    return data
+
 def process_data(data):
     """
     Process data. Expects a list of lists with numbers.
@@ -10,11 +36,6 @@ def process_data(data):
     """
     # Convert to Numpy float32
     data = np.array(data, np.float32)
-
-    # Normalise data
-    if data == -9999:
-        data = 0
-    else: data /= 9999
 
     # Rotate the array by 90 degrees
     data = np.rot90(data)