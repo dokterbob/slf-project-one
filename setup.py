d1951085f60f2d91d5ab0b42d83fbd5733bfa706
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2014-01-10 18:06:02 +0100

    Configure project_one as a script.
    This automatically runs main() when typing 'project_one' in the command line (after installing).

--- setup.py
+++ setup.py
@@ -6,4 +6,13 @@ setup(
     packages = find_packages(),
     license = 'BSD',
     long_description = open('README.rst').read(),
+
+    # This causes the main function in project_one to be run when the
+    # project_one command is executed.
+    # Use this when setup.py should install an executable command.
+    entry_points={
+        'console_scripts': [
+            'project_one = project_one.project_one:main',
+        ],
+    },
 )
