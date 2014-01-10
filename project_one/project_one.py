3a62a12c030051ba18b301bb1bb961a172bff748
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2014-01-10 18:05:25 +0100

    Run main code from within a function.

--- project_one/project_one.py
+++ project_one/project_one.py
@@ -1,10 +1,22 @@
+# System imports first
+import sys
+
+# Module (local) imports
 from import_data import import_data
 from process import process_data, normalize, rotate_data
 from output import plot_data
 
-# Read the data
-data = import_data('data.txt')
-data = process_data(data)
-# data = normalize(data)
-# data = rotate_data(data)
-plot_data(data)
+
+def main(argv=None):
+    """ Main function, executed when running 'project_one'. """
+    # Read the data
+    data = import_data('data.txt')
+    data = process_data(data)
+    # data = normalize(data)
+    # data = rotate_data(data)
+    plot_data(data)
+
+# If we're being run with `python project_one.py`, execute main() and exit
+# afterwards with the return value of main().
+if __name__ == "__main__":
+    sys.exit(main())
